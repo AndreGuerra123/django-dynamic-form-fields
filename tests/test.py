ccb66abb27d6960f4d950b2867406a899fabdce4
André Catarino Guerra <guerraandre@hotmail.com>
2018-11-06 13:10:58 +0000

    Create test.py

---
+++ tests/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+
+
+class TestCaseOne(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
