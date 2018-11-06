402f4574293a4ad647b315206def0903720f6fe7
André Catarino Guerra <guerraandre@hotmail.com>
2018-11-06 02:15:14 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,4 @@
-import os
 import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dynamic_fields.settings")
 from django.core.management import execute_from_command_line
 execute_from_command_line(sys.argv)
