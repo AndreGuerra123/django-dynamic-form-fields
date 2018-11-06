37eb0b9a5f0d74db28c75ee54533dec2b6287c68
André Catarino Guerra <guerraandre@hotmail.com>
2018-11-06 03:27:23 +0000

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -5,7 +5,7 @@ class TestRunner(TestCase):
    def test_main_view_get(self):
        c = Client()
        get_response = c.get('/choices/')
-       self.assertEqual(get_response.status_code,200)
+       self.assertEqual(get_response.status_code,2011)
 
 def runtests():
     TestRunner()
