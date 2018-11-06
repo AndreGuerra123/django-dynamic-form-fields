4bd880ec09691423849a720d23dd21ad8e55b827
André Catarino Guerra <guerraandre@hotmail.com>
2018-11-06 03:29:39 +0000

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -2,13 +2,15 @@ from django.test import TestCase
 from django.test import Client
 
 class TestRunner(TestCase):
+   c = Client()
+
    def test_main_view_get(self):
-       c = Client()
-       get_response = c.get('/choices/')
+       get_response = self.c.get('/choices/')
        self.assertEqual(get_response.status_code,2011)
 
 def runtests():
-    TestRunner()
+    runner = TestRunner()
+    runner.test_main_view_get()
     
 if __name__ == '__main__':
     runtests()
